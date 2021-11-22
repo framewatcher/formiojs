4b6995732b26429687ac54f850d82ce6c26c9067
Mohd Fazli Bin Jafrin <fazli@digiheritage.com.my>
2021-11-22 14:24:51 +0800

    add all

--- formiogithubjs.js
+++ formiogithubjs.js
@@ -1,2 +1,4 @@
-$("body > div > div ").html('');
-$("body > div > div ").append('<a href="#/form/619afa8b529306d257bb681e/"><h5>Admin</h5></a>');
+$("body > nav > div > div.navbar-header").dblclick(function(){
+  $("body > div > div ").html('');
+  $("body > div > div ").append('<a href="#/form/619afa8b529306d257bb681e/"><h5>Admin</h5></a>');
+});
